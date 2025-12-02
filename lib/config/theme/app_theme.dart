0cbf4db37da80818dadc559e10c21df83b1b8bb2
ErikDev <158068793+Erik-egam@users.noreply.github.com>
2025-12-02 12:01:28 -0500

    appBar

---
+++ lib/config/theme/app_theme.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+const _colorList = <Color>[
+  Colors.greenAccent,
+  Colors.cyan,
+  Colors.deepOrangeAccent,
+  Colors.teal,
+  Colors.tealAccent,
+  Colors.pinkAccent,
+  Colors.blueGrey,
+  Colors.lightGreenAccent,
+];
+
+
+class AppTheme {
+  final int selectedColor;
+
+  AppTheme({this.selectedColor = 0}): assert (selectedColor >= 0, "Color must be greater than 0"),
+  assert (selectedColor<_colorList.length, "Color must be less than ${_colorList.length}");
+  
+  ThemeData getTheme(){
+    return ThemeData(
+      // brightness: Brightness.dark,
+      appBarTheme: AppBarTheme(centerTitle: true,),
+      useMaterial3: true,
+      colorSchemeSeed: _colorList[selectedColor] 
+    );
+  }
+}

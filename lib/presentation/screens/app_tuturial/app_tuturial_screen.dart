00fb671875bc3e1dd7d020bfbf05228e93e0b93f
ErikDev <158068793+Erik-egam@users.noreply.github.com>
2025-12-03 11:03:43 -0500

    screens creadas y barril actualizado

---
+++ lib/presentation/screens/app_tuturial/app_tuturial_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppTuturialScreen extends StatelessWidget {
+  const AppTuturialScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
